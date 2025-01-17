4f8a7f6281fd72e85240a65a77dbcef5809d1f0e
Amber <amber.julian@untamedisles.com>
2025-01-17 13:06:36 +1100

    update: got unowned properties storing in a hash. Doesn't include Go because it isn't an ownable property.

--- ApplicationFiles/Code/PropertyTracker.rb
+++ ApplicationFiles/Code/PropertyTracker.rb
@@ -1,8 +1,37 @@
-class Test 
+#importing the JSON Module 
+require 'json'
+
+
+class PropertyTracker 
+#Example of how to get information out
+#data = '{"Name" : "Harsha" ,"Subject":{"1":"Physics","2":"Maths"}}'
+#json_string = JSON.parse(data)
+#puts json_string["Name"]
+  UNOWNEDPROPERTYKEY = -1 #Constants begin with capital letters. The first example I saw had the declaration totally uppercase, so I've done that here. 
+  unownedPropertyHash = {}
+
+  #Ruby has a built in initialize method, for setting default values
   def initialize
-   puts "Hello World"
-  end
+   # puts "Hello World" # this is how to console.log
+
+   file = File.open "../Data/board.json"
+   data = JSON.load file
+   #puts data
+   
+    
+   data.each do |property|
+    if "#{property["type"]}" == "property"
+      #puts "#{property["type"]}"
+      oropertyOwnershipHash = {:"#{property["name"]}" => -1}
+      puts unownedPropertyHash
+    end
+    #if "#{property["type"]}" == "property"
+   # puts "#{property["name"]}"
+   end
+  
+  
+  end #End closes a block. I think it might be similar to return. 
 end
 
 # initialize object
-Test.new
+PropertyTracker.new

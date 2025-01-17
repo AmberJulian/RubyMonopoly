87c2e983f1088bba763b5d94143b47952d43f7a9
Amber <amber.julian@untamedisles.com>
2025-01-17 14:03:01 +1100

    update: remove some unneccessary setters in player.rb because I realized you can do setters in the built in initialize function

--- ApplicationFiles/Code/Player/player.rb
+++ ApplicationFiles/Code/Player/player.rb
@@ -8,12 +8,4 @@ class Player
         id = id
         puts "#{name} made with id #{id}"
     end
-
-    def setName(newName)
-       
-    end
-
-    def setID(id)
-        playerId = id #If this was a game connected to google store or whatever, it may make sense for this to be their store ID. Since I'm not sure how the app wants to handle it, Ill just make a setter.
-    end
 end

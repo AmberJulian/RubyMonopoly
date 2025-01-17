ebd4a58baa1f4ad923034abe627572b895227715
Amber <amber.julian@untamedisles.com>
2025-01-17 14:39:48 +1100

    update: found out that in order to access variables from other files we use an attribute reader and an @ symbol

--- ApplicationFiles/Code/Player/player.rb
+++ ApplicationFiles/Code/Player/player.rb
@@ -1,11 +1,13 @@
-
-
 class Player
+    attr_reader :location, :money, :name, :id
+
+#TODO: Remove these attribute readers - debug only 
+
     def initialize(playerName, id)
-        location = 0
-        money = 16
-        name = playerName
-        id = id
-        puts "#{name} made with id #{id}"
+        @location = 0
+        @money = 16
+        @name = playerName
+        @id = id
+        puts "#{@name} made with id #{@id}"
     end
 end
--- ApplicationFiles/Code/Player/playerManager.rb
+++ ApplicationFiles/Code/Player/playerManager.rb
@@ -3,18 +3,20 @@ require 'json'
 require_relative 'player'
 
 class PlayerManager
-
+    attr_reader :players # Expose players via an accessor
+  
     #initialize all the players with names in our datafile, aka, all the ones given to us
     def initialize 
-
+        @players = {} # Instance variable for storing players
         file = File.open "../../Data/players.json"
         playerData = JSON.load file
-     
-        puts playerData.count
          
         for i in 0..(playerData.count-1)
-            newPlayer = Player.new(playerData[i],i)   
+            newPlayer = Player.new(playerData[i],i)  
+            @players[i] = newPlayer 
         end
+
+        puts "#{@players[2].name}"
     end
 end
 
